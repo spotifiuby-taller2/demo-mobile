7bc364863f5ea9ff1e5a43405881d6f689e816dd
Esteban <edjeordjian@fi.uba.ar>
2022-03-20 18:38:31 -0300

    Changed file

--- src/Components/SignInGoogleButton.js
+++ src/Components/SignInGoogleButton.js
@@ -8,6 +8,8 @@ import {
 import React, { useState } from 'react'
 import { Button } from 'react-native-paper'
 import constants from "../others/constants"
+import StyledFirebaseAuth from 'firebaseui/StyledFirebaseAuth';
+
   
 export default SignInWithGoogle = () =>{
 
