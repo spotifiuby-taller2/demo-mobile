19fc7c49cee93c01dcd38ca3f8341ddf3e237568
Esteban <edjeordjian@fi.uba.ar>
2022-03-24 18:02:48 -0300

    First changes to fix firebase

--- src/Components/SignInGoogleButton.js
+++ src/Components/SignInGoogleButton.js
@@ -10,10 +10,12 @@ import { Button } from 'react-native-paper'
 import * as Google from 'expo-google-app-auth';
 import {IOS_KEY, ANDROID_KEY, WEB_KEY} from "@env"
 import constants from '../others/constants'
+const {getAuth,signInWithCredential, GoogleAuthProvider} = require("firebase/auth");
 
 export default SignInWithGoogle = () =>{
      let handleSignInWithGoogle = async () => {
         let result;
+
         try {
             result = await Google.logInAsync({ //return an object with result token and user
             iosClientId: IOS_KEY, //From app.json
@@ -25,29 +27,40 @@ export default SignInWithGoogle = () =>{
           alert('login: Error:' + message);
         }
 
-        if (result.type === 'success') {         
+         if (result.type !== 'success') {
+             alert("No pudo autenticarse al usuario con Google");
+             return;
+         }
 
-            fetch(constants.USERS_HOST + constants.SIGN_IN_URL,
-                {
-                  method: 'POST',
-                  headers: constants.JSON_HEADER,
-                  body: JSON.stringify({
-                      token: result,
-                      link: "mobile",
-                      firebase: true
-                  })
+         const credential = GoogleAuthProvider.credential(result.idToken,
+                                                          result.accessToken);
 
-            })
-            .then((res) => res.json())
-            .then((response)=>{
-                    
-                console.log(response);
-            })
-            .catch((err)=>{alert(err)});
-            
-        } else {
-          alert("No pudo autenticarse al usuario con Google");
-        }
+         const auth = getAuth();
+
+         const response = await signInWithCredential(auth, credential);
+
+         if (response.user === undefined) {
+             alert("No pudo autenticarse al usuario con Google");
+             return;
+         }
+
+        fetch(constants.USERS_HOST + constants.SIGN_IN_URL,
+            {
+              method: 'POST',
+              headers: constants.JSON_HEADER,
+              body: JSON.stringify({
+                  token: response,
+                  link: "mobile",
+                  firebase: true
+              })
+
+        })
+        .then((res) => res.json())
+        .then((response)=>{
+
+            console.log(response);
+        })
+        .catch((err)=>{alert(err)});
       };
 
 
