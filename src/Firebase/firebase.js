c12b3541c8117f2e519b3fddb7d84cb1ad8c9349
alfo777 <alfonso71292@gmail.com>
2022-03-27 14:44:33 -0300

    firebase add to deploy

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,7 +2,10 @@ import { initializeApp } from 'firebase/app'
 import {getAuth} from 'firebase/auth'
 
 
-const firebaseConfig = {
+
+
+
+const firebaseConfigDev = {
     apiKey: "AIzaSyDlFbw1n3eqg7ogdwGuiTetV6isK4Uhqno",
     authDomain: "fir-firebase-acc6b.firebaseapp.com",
     projectId: "fir-firebase-acc6b",
@@ -10,6 +13,17 @@ const firebaseConfig = {
     messagingSenderId: "296878360901",
     appId: "1:296878360901:web:7987ce42ec0a406b1f162c"
   };
+
+  const firebaseConfigProd = {
+    apiKey: "AIzaSyCnDa9J7DKKtNv5crxZ4NrRGcW5c7nZTAg",
+    authDomain: "fir-firebase-2-9eb22.firebaseapp.com",
+    projectId: "fir-firebase-2-9eb22",
+    storageBucket: "fir-firebase-2-9eb22.appspot.com",
+    messagingSenderId: "701624425016",
+    appId: "1:701624425016:web:6cb2157c5a2c0a34e1a4cd"
+  };
+
+const firebaseConfig = (__DEV__)? firebaseConfigDev : firebaseConfigProd;
   
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
