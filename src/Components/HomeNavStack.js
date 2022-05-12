a8f1f134e82bcadf29ffab9944d7c047e60a1e41
Fernando Sinisi <fernando.sinisi@despegar.com>
2022-05-11 23:29:19 -0300

    borrar cosas sin uso
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- src/Components/HomeNavStack.js
+++ src/Components/HomeNavStack.js
@@ -51,39 +51,3 @@ export default HomeNavStack = () =>{
         </HomeStack.Navigator>
     )
 }
-
-      const styles = StyleSheet.create(
-        { input: {
-            borderWidth: 2, 
-            marginBottom: 15,
-            marginTop: 15,
-            backgroundColor: '#f5fcff',
-            height: 50
-           },
-          container: {
-            flex:1,
-            backgroundColor: '#f5fcff',
-            paddingLeft: 15,
-            paddingRight: 15,
-            marginTop: 30
-           },
-           title: {textAlign: 'center',fontSize: 25, marginBottom: 35},
-           button: {
-             backgroundColor: 'skyblue', 
-             paddingTop: 15, 
-             paddingBottom:15, 
-             width: 100, 
-             alignSelf: 'center', 
-             marginTop: 30, 
-             marginBottom:30,
-             borderRadius: 10},
-           buttonText: {textAlign: 'center', fontSize: 13},
-           forgotPasswordButton: {textAlign: 'center', fontSize: 13, color: 'skyblue'},
-           image:{
-             height: 170,
-             width: 170,
-             alignSelf: 'center',
-             marginTop: 50,
-             marginBottom: 80
-           }}
-     )
