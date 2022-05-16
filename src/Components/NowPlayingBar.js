ece8c524e8cb6b414d79d5919f7a51ccc354bdac
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2022-05-16 00:33:43 -0300

    temporarily use demo songs from the other firebase

--- src/Components/NowPlayingBar.js
+++ src/Components/NowPlayingBar.js
@@ -17,21 +17,21 @@ import SwipeableView from "./SwipeableView";
 const tracks = [
   {
     id: 1,
-    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-2-9eb22.appspot.com/o/y2mate.com%20-%20Cuando%20me%20acuerdo%20de%20Salta%20Los%20Chalchaleros.mp3?alt=media&token=4f05d90c-509a-4435-8e2f-d361c30c0909',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-acc6b.appspot.com/o/y2mate.com%20-%20Cuando%20me%20acuerdo%20de%20Salta%20Los%20Chalchaleros.mp3?alt=media&token=fcbf582d-5c4e-4b1e-99b6-f213a0195ec0',
     title: 'Cuando me acuerdo de Salta',
     artist: 'Los Chalchaleros',
     artwork: 'https://www.cmtv.com.ar/tapas-cd/chalchadel78.jpg',
   },
   {
     id: 2,
-    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-2-9eb22.appspot.com/o/Eric%20Clapton-%20Wonderful%20Tonight%20(HQ).mp3?alt=media&token=c1f26352-8c9d-45cd-a22a-b2032c283e38',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-acc6b.appspot.com/o/Eric%20Clapton-%20Wonderful%20Tonight%20(HQ).mp3?alt=media&token=9e58b0b6-5353-4699-aca7-45039c48484d',
     title: 'Wonderful tonight',
     artist: 'Eric Clapton',
     artwork: 'https://i1.sndcdn.com/artworks-000429269373-ogan3j-t500x500.jpg',
   },
   {
     id: 3,
-    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-2-9eb22.appspot.com/o/The%20Gambler.mp3?alt=media&token=1d4e203b-e654-4afe-ae4f-04e96959475a',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-firebase-acc6b.appspot.com/o/The%20Gambler.mp3?alt=media&token=31184358-d04c-471f-8fd6-6e8bfe3e38cd',
     title: 'The Gambler',
     artist: 'Kenny Rogers',
     artwork: 'https://upload.wikimedia.org/wikipedia/en/b/be/The_Gambler_-_Kenny_Rogers.jpg',
