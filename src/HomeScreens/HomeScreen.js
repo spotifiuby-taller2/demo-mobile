959f16ed811585a1326dc3050964dda0a85c3b4e
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2022-05-21 16:35:06 -0300

    use song list in home

--- src/HomeScreens/HomeScreen.js
+++ src/HomeScreens/HomeScreen.js
@@ -1,64 +1,60 @@
-import {SafeAreaView, View, ScrollView, StyleSheet} from 'react-native';
-import React, {useEffect, useState, useRef} from 'react'
-import { useAuthUser } from '../context/AuthContext';
+import React, {useEffect, useState} from 'react'
+import {useAuthUser} from '../context/AuthContext';
 import {getToGateway, postToGateway} from "../others/utils";
 import constants from "../others/constants";
-import {containerStyle} from "../styles/genericStyles";
-import SongListItem from "../Components/SongListItem";
 import * as Notifications from 'expo-notifications';
+import SongList from "../Components/SongList";
 
 const HomeScreen = ({navigation}) => {
   const [songs, setSongs] = useState([]);
 
   const {userState} = useAuthUser();
 
-  const {signOut} = useAuthUser();
+  const getFavoriteSongs = async () => {
+    await getToGateway(constants.MEDIA_HOST + constants.FAVORITE_SONGS
+      + "?"
+      + constants.USER_ID_QUERY_PARAM
+      + userState.uid)
+      .then((response) => {
+        if (response.error !== undefined) {
+          alert(response.error);
+          return;
+        }
 
-    const getFavoriteSongs = async () => {
-        await getToGateway(constants.MEDIA_HOST + constants.FAVORITE_SONGS
-            + "?"
-            + constants.USER_ID_QUERY_PARAM
-            + userState.uid)
-            .then( (response) => {
-                if (response.error !== undefined) {
-                    alert(response.error);
-                    return;
-                }
+        setSongs(response.songs);
+      });
+  }
 
-                setSongs(response.songs);
-            } );
-    }
+  useEffect(() => {
+    (async () => {
+      await getFavoriteSongs();
+    })();
 
-  useEffect( () => {
-      (async () => {
-          await getFavoriteSongs();
-      })();
-
-      return () => {
-      };
+    return () => {
+    };
   }, []);
 
-  useEffect( () => {
-      navigation.addListener('focus', async () => {
-          await getFavoriteSongs();
-      }, [navigation]);
+  useEffect(() => {
+    navigation.addListener('focus', async () => {
+      await getFavoriteSongs();
+    }, [navigation]);
 
-      const subcriptionNotificationReceived = Notifications.addNotificationReceivedListener(
-          async notification=> {
-              const {type, params, screenName} = notification.request.content.data;
-              const body = {
-                idEmissor: params.idEmissor,
-                idReceptor: params.idReceptor,
-                redirectTo: constants.USERS_HOST + constants.NOTIFICATION_LIST_URL
-              }
+    const subcriptionNotificationReceived = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const {type, params, screenName} = notification.request.content.data;
+        const body = {
+          idEmissor: params.idEmissor,
+          idReceptor: params.idReceptor,
+          redirectTo: constants.USERS_HOST + constants.NOTIFICATION_LIST_URL
+        }
 
-              await postToGateway(body, 'POST')
-                .then(res => {
-                  if ( res.error !== undefined && res.error !== constants.DUPLICATE_NOTIFICATION_ERROR  ){
-                    alert(res.error);
-                  }
-                })
-         });
+        await postToGateway(body, 'POST')
+          .then(res => {
+            if (res.error !== undefined && res.error !== constants.DUPLICATE_NOTIFICATION_ERROR) {
+              alert(res.error);
+            }
+          })
+      });
 
     const subcriptionNotificationClicked = Notifications.addNotificationResponseReceivedListener(notification => {
       const {type, params, screenName} = notification.notification.request.content.data;
@@ -72,65 +68,9 @@ const HomeScreen = ({navigation}) => {
     };
   }, [navigation]);
 
-
-    return (
-        <View style={containerStyle}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    {
-                        (songs !== undefined) && (Array.isArray(songs)) && (
-                            songs.map( (song, id) => {
-                                return (
-                                    <SongListItem id={id}
-                                                  key={id}
-                                                  track={song}
-                                                  navigation={navigation}/>
-                                )
-                            } ) )
-                    }
-                </View>
-            </ScrollView>
-        </View>
-    )
+  return <SongList songList={songs} navigation={navigation}/>
 }
 
-    const styles = StyleSheet.create(
-      { input: {
-          borderWidth: 2,
-          marginBottom: 15,
-          marginTop: 15,
-          backgroundColor: '#f5fcff',
-          height: 50
-         },
-        container: {
-          flex:1,
-          backgroundColor: '#f5fcff',
-          paddingLeft: 15,
-          paddingRight: 15,
-          marginTop: 30
-         },
-         title: {textAlign: 'center',fontSize: 25, marginBottom: 35},
-         button: {
-           backgroundColor: 'skyblue',
-           paddingTop: 15,
-           paddingBottom:15,
-           width: 100,
-           alignSelf: 'center',
-           marginTop: 30,
-           marginBottom:30,
-           borderRadius: 10},
-         buttonText: {textAlign: 'center', fontSize: 13},
-         forgotPasswordButton: {textAlign: 'center', fontSize: 13, color: 'skyblue'},
-         image:{
-           height: 170,
-           width: 170,
-           alignSelf: 'center',
-           marginTop: 50,
-           marginBottom: 80
-         }}
-   );
-
-
-    export {
-        HomeScreen
-    }
+export {
+  HomeScreen
+}
