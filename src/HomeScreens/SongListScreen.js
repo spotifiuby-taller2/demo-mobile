55f0a82e066eb9f3dd6de924d34e11f34e044325
Esteban <edjeordjian@fi.uba.ar>
2022-05-22 02:56:27 -0300

    Minor fix on song list

--- src/HomeScreens/SongListScreen.js
+++ src/HomeScreens/SongListScreen.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {getAllSongs} from "../Services/MediaService";
 import SongList from "../Components/SongList";
+import {getToGateway} from "../others/utils";
+import * as constants from "../others/constants";
 
 const SongListScreen = ({navigation}) => {
   const [allSongs, setAllSongs] = useState([]);
@@ -8,6 +10,12 @@ const SongListScreen = ({navigation}) => {
   useEffect(() => {
     getAllSongs().then(setAllSongs);
   }, []);
+
+  useEffect( () => {
+    navigation.addListener('focus', async () => {
+      getAllSongs().then(setAllSongs);
+    }, [navigation]);
+  }, [navigation] );
 
   return <SongList songList={allSongs} navigation={navigation}/>;
 }
