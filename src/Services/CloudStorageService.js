c9b6e83de1c27df9ae6a5f0060c0b459b3b2837c
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2022-05-30 22:49:19 -0300

    unify firebase storage handling

--- src/Services/CloudStorageService.js
+++ src/Services/CloudStorageService.js
@@ -3,7 +3,6 @@ import {getDownloadURL, getStorage, ref, uploadBytes} from 'firebase/storage';
 
 const storage = getStorage(app);
 
-//video and music
 const uploadFile = (file, name) => {
   const fileRef = ref(storage, name);
   return uploadBytes(fileRef, file)
@@ -14,23 +13,4 @@ const uploadFile = (file, name) => {
     });
 }
 
-//image
-const uploadImage = async (file, name) => {
-  const blob = await new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-      resolve(xhr.response);
-    };
-    xhr.onerror = function (e) {
-      reject(new TypeError("Network request failed"));
-    };
-    xhr.responseType = "blob";
-    xhr.open("GET", file, true);
-    xhr.send(null);
-  });
-  let storageRef = ref(storage, name);
-  await uploadBytes(storageRef, blob);
-  return getDownloadURL(storageRef);
-}
-
-export {uploadFile, uploadImage};
+export {uploadFile};
